1b070697101f890b7c62adfb8738c697dea56796
Stefan Sörqvist <stefan@aigoo.se>
2018-09-03 08:44:31 +0200

    Added support for multiple themedays and added irc-topic command

--- main.php
+++ main.php
@@ -15,7 +15,21 @@ if ($curl->error) {
 // TODO: Maybe deliver some options, different formats or similar
 $html = HtmlDomParser::str_get_html($curl->response);
 $elems = $html->find('div[class=content]', 0);
-$themedayOutertext = $elems->find('center', 0)->outertext;
-$themedayWTags = strip_tags($themedayOutertext);
-$themeday = str_replace("...", ": ", $themedayWTags);
+$themedayInnertext = $elems->find('center', 0)->innertext;
+$outputArrayed = array_filter(explode("<br />", $themedayInnertext));
+$output = "";
+foreach ($outputArrayed as $i => $row) {
+    if ($i == 0) {
+        $output .= strip_tags($row);
+    } else if ($i == count($outputArrayed) - 1) {
+        $output .= $row ." & ";
+    } else {
+        $output .= $row;
+    }
+}
+$themeday = str_replace("...", ": ", $output);
+if (isset($argv[1]) == "irc") {
+    echo "/topic {$themeday}\n";
+    exit;
+}
 echo $themeday."\n";
