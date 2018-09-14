627b181006588834eb92010698da3b0bc27eb896
Stefan Sörqvist <stefan@aigoo.se>
2018-09-14 20:01:29 +0200

    Some changes were made, now it behaves more properly...

--- main.php
+++ main.php
@@ -15,16 +15,24 @@ if ($curl->error) {
 // TODO: Maybe deliver some options, different formats or similar
 $html = HtmlDomParser::str_get_html($curl->response);
 $elems = $html->find('div[class=content]', 0);
+if (!$elems->find('center', 0)) {
+    if (isset($argv[1]) == "irc") {
+        echo "/topic Idag firar vi : Ingenting\n";
+        exit;
+    }
+    echo "Idag firar vi : Ingenting\n";
+    exit;
+}
 $themedayInnertext = $elems->find('center', 0)->innertext;
-$outputArrayed = array_filter(explode("<br />", $themedayInnertext));
+$outputArrayed = array_values(array_filter(explode("<br />", $themedayInnertext)));
 $output = "";
 foreach ($outputArrayed as $i => $row) {
     if ($i == 0) {
         $output .= strip_tags($row);
     } else if ($i == count($outputArrayed) - 1) {
-        $output .= $row ." & ";
+        $output .= strip_tags($row);
     } else {
-        $output .= $row;
+        $output .= strip_tags($row) ." & ";
     }
 }
 $themeday = str_replace("...", ": ", $output);
